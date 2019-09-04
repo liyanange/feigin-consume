3e59d998dcbc7a75f5dd472e6b44410e0734ca55
liyanan <1215423048@qq.com>
2019-09-04 15:54:44 +0800

    kankaknka

--- src/main/java/com/example/cloudprovide/RemoteClient.java
+++ src/main/java/com/example/cloudprovide/RemoteClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("ServiceApp1 remotetee")
+@FeignClient("ServiceApp1 remotetee2222222222222")
 public interface RemoteClient {
     @RequestMapping(method = RequestMethod.GET, value = "/test")
     String test();
